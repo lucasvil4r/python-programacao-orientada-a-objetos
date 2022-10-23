172704279315fa8d809d0f0401b4dabe99830f70
lucasvil4r <lucasvilar-celestino@hotmail.com>
2022-10-23 19:08:35 -0300

    Questao 2

---
+++ Atividade_contínua_04/questao_02.py
@@ -0,0 +1,14 @@
+'''
+Assinale a alternativa que completa corretamente a afirmativa abaixo:
+
+Uma superclasse é uma classe cuja finalidade principal é definir
+uma estrutura comum para suas subclasses.
+Os métodos que uma classe abstrata declara, mas não implementa,
+são chamados de métodos abstratos.
+
+classe mãe / concreta / concretos
+classe concreta / abstrata / abstratos
+classe abstrata / abstrata / abstratos
+superclasse / abstrata / abstratos
+classe abstrata / concreta / concretos
+'''
