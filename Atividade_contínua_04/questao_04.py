8ce845d38f3bda6448b2e79ec708702144b7ca36
lucasvil4r <lucasvilar-celestino@hotmail.com>
2022-10-23 19:08:51 -0300

    Questao 4

---
+++ Atividade_contínua_04/questao_04.py
@@ -0,0 +1,18 @@
+'''
+A Programação Orientada a Objeto não é apenas uma forma de programar, é também um jeito de pensar em um problema utilizando conceitos do mundo real e, não somente conceitos computacionais.
+Considerando os conceitos da Orientação a Objetos, analise as afirmações abaixo e assinale a alternativa CORRETA:
+
+I. O objeto tem determinadas propriedades que o caracterizam e que são armazenadas no próprio objeto. As propriedades de um objetos são chamadas de instâncias.
+
+II. As mensagens são informações enviadas ao objeto para que ele se comporte de uma determinada maneira. São os métodos de um objeto que determinam como eles irão se comportar ao receber uma mensagem.
+
+III. A herança é um mecanismo para o compartilhamento de métodos e atributos entre classes e subclasses, permitindo a criação de novas.
+
+IV. O encapsulamento é um mecanismo que permite o acesso aos dados de um objeto somente através dos métodos públicos desse objeto. Nenhuma outra parte do programa pode operar sobre os dados privados do objeto.
+
+V. A abstração é caracterizada pela capacidade de poder definir classes abstratas em um programa.
+
+VI. Devido ao polimorfismo, a mesma chamada de método pode, em momentos diferentes, executar métodos diferentes, dependendo do tipo da variável utilizada para fazer essa chamada.
+
+'''
+
