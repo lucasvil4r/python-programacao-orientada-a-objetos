0c799196665dba7df8ca396cb10c74b4d614bfae
lucasvil4r <lucasvilar-celestino@hotmail.com>
2022-11-06 14:41:56 -0300

    atividade continua 5

--- Atividade_contínua_05/ac05_filme.py
+++ Atividade_contínua_05/ac05_filme.py
@@ -1,12 +1,9 @@
 # ATIVIDADE CONTÍNUA 05
 
 # NOMES DOS ALUNOS: (MÁXIMO 6):
-# ALUNO 1: nome
-# ALUNO 2: nome
-# ALUNO 3: nome
-# ALUNO 4: nome
-# ALUNO 5: nome
-# ALUNO 6: nome
+# Lucas Vilar - 2101658
+# Gustavo R. Gatto - RA:2201041
+# Gustavo Gonçalves Rodrigues - 2200601
 
 # IMPORTAR MÓDULOS
 from sqlalchemy import create_engine, Column, Integer, String, Float
@@ -35,13 +32,14 @@ connection.execute('''CREATE TABLE IF NOT EXISTS FILME(
 
 # Implementar classe Filme e realizar o mapeamento da tabela
 
+
 class Filme(Base):
     __tablename__ = 'FILME'
     id = Column('ID', Integer, primary_key=True, autoincrement=True)
     titulo = Column('TITULO', String(255))
     ano = Column('ANO', Integer)
     duracao = Column('DURACAO', Integer)
-    pais = Column('PAIS',String(255))
+    pais = Column('PAIS', String(255))
     diretor = Column('DIRETOR', String(255))
     avaliacao = Column('AVALIACAO', Float)
 
@@ -53,50 +51,61 @@ class Filme(Base):
         self.diretor = diretor
         self.avaliacao = avaliacao
 
-# Importar filmes do arquivo de texto movies.txt e inserir no banco de dados
-# Formato do arquivo: 
-# titulo;ano;duracao;pais;diretor;avaliacao
-arquivo = open('movies.txt', 'r', encoding='UTF-8')
 
-lista_filmes = []
-for linha in arquivo:
+# Importar filmes do file de texto movies.txt e inserir no banco de dados
+# Formato do file:
+# titulo;ano;duracao;pais;diretor;avaliacao
+file = open('./Atividade_contínua_05/movies.txt', 'r', encoding='UTF-8')
+
+list_movies = []
+for linha in file:
     lista = linha.split(';')
-    filmes = Filme(lista[0], int(lista[1]), int(lista[2]), lista[3], lista[4], float(lista[5]))
-    lista_filmes.append(filmes)
-session.add_all(lista_filmes)
-session.commit()
+    filmes = Filme(lista[0], int(lista[1]), int(lista[2]),
+                   lista[3], lista[4], float(lista[5]))
+    list_movies.append(filmes)
+session.add_all(list_movies)
+# session.commit()
 
 # Consultar todos os filmes e ordenar pelo título
-resultado = session.query(Filme).order_by(Filme.titulo).group_by(Filme.titulo)
-for filme in resultado:
+query = session.query(Filme).order_by(Filme.titulo).group_by(Filme.titulo)
+for filme in query:
     print(f'{filme.titulo} | {filme.ano} | {filme.duracao} | {filme.pais} | {filme.diretor} | {filme.avaliacao}')
+
+print('-' * 100)
 
 # Consultar filmes do ano de 2020 e ordenar pelo título
-print('-' * 130)
-
-resultado = session.query(Filme).filter(Filme.ano == 2020).order_by(Filme.titulo).group_by(Filme.titulo)
-for filme in resultado:
+query = session.query(Filme).filter(Filme.ano == 2020).order_by(
+    Filme.titulo).group_by(Filme.titulo)
+for filme in query:
     print(f'{filme.titulo} | {filme.ano} | {filme.duracao} | {filme.pais} | {filme.diretor} | {filme.avaliacao}')
+
+print('-' * 100)
 
 # Consultar filmes de 2019 com avaliação superior a 80 e ordenar pelo título
-print('-' * 130)
-
-resultado = session.query(Filme).filter(Filme.ano == 2019, Filme.avaliacao > 80).order_by(Filme.titulo).group_by(Filme.titulo)
-for filme in resultado:
+query = session.query(Filme).filter(Filme.ano == 2019, Filme.avaliacao > 80).order_by(
+    Filme.titulo).group_by(Filme.titulo)
+for filme in query:
     print(f'{filme.titulo} | {filme.ano} | {filme.duracao} | {filme.pais} | {filme.diretor} | {filme.avaliacao}')
 
+print('-' * 100)
+
 # Excluir todos os filmes do ano de 2020
-resultado = session.query(Filme).filter(Filme.ano == 2020)
-for filme in resultado:
+query = session.query(Filme).filter(Filme.ano == 2020)
+for filme in query:
     session.delete(filme)
-    session.commit()
+    # session.commit()
+
+print('Excluindo todos os filmes do ano de 2020')
 
 # Exportar filmes para um arquivo de texto, ordenados pelo título e no formato:
 # título;ano;duracao;país;diretor;avaliacao
-resultado = session.query(Filme)
-arquivo = open('movies_2.txt', 'w', encoding='UTF-8')
-for filmes in resultado:
-    arquivo.write(f'{filmes.titulo};{str(filmes.ano)};{str(filmes.duracao)};{filmes.pais};{filmes.diretor};{str(filmes.avaliacao)} \n')
+query = session.query(Filme)
+file = open('./Atividade_contínua_05/texto.txt', 'w', encoding='UTF-8')
+for filmes in query:
+    file.write(
+        f'{filmes.titulo};{str(filmes.ano)};{str(filmes.duracao)};{filmes.pais};{filmes.diretor};{str(filmes.avaliacao)}\n')
 
-arquivo.close()
+print('Exportar filmes para um arquivo de texto')
+
+file.close()
 connection.close()
